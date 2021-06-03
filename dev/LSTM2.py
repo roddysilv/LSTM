c55bce19753466e6c452735ab1590760120dbe5f
Rodrigo Oliveira <rodrigo.silvoliveira@gmail.com>
2021-06-03 14:36:55 -0300

    Update LSTM2.py

--- dev/LSTM2.py
+++ dev/LSTM2.py
@@ -5,7 +5,6 @@ from pandas import read_csv
 from pandas import DataFrame
 from pandas import concat
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import mean_squared_error
 from keras.models import Sequential
 from keras.layers import Dense
