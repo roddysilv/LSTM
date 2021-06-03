89dbddf8a378f0251184ca380a5b8e861de36ca4
Rodrigo Oliveira <42419004+roddysilv@users.noreply.github.com>
2021-06-03 14:15:05 -0300

    Atualiza forma de entrada de dados na rede

--- dev/LSTM2.py
+++ dev/LSTM2.py
@@ -1,196 +1,118 @@
-import numpy
-import matplotlib.pyplot as plt
-import pandas as pd
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  1 22:30:21 2021
+
+@author: Rodrigo
+"""
+
+from math import sqrt
+from numpy import concatenate
+from matplotlib import pyplot
+from pandas import read_csv
+from pandas import DataFrame
+from pandas import concat
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import mean_squared_error
 from keras.models import Sequential
-from keras.layers import Dense 
+from keras.layers import Dense
 from keras.layers import LSTM
 from keras.callbacks import EarlyStopping
-from sklearn.preprocessing import MinMaxScaler
-import sys
-import time
-import datetime
-
-numpy.random.seed(5)
-
-#%%
-#Convertendo o array em uma matriz de dados
-def create_dataset(dataset, input_shape, look_back=1):
-	dataX, dataY = [], []
-	for i in range(len(dataset)-look_back-1):
-		a = dataset[i:(i+look_back),:input_shape]
-		dataX.append(a)
-		dataY.append(dataset[i + look_back, input_shape-1])
-	return numpy.array(dataX), numpy.array(dataY)
-
-#%%
-#Carregando os dados
-dataframe = pd.read_csv('diario/Residential_1_daily_sum.csv',index_col=('date'), engine='python')
-dataframe.boxplot(column=['energy_kWh'])
-
-#%%
-use_dummie = False
-
-if use_dummie: 
-    dummie = pd.get_dummies(dataframe.pop('weather'))
-    dataframe = pd.concat([dummie,dataframe],axis=1)
-else:
-    pass
-    # dataframe.pop('weather')
-    
-input_shape = dataframe.shape[1]
-
-#%%
-dataset = dataframe.values
-dataset = dataset.astype('float32')
-
-#%%
-print('Média:        ', dataset[:,-1].mean())
-print('Desvio Padrão:', dataset[:,-1].std())
-print('Max:         :',dataset[:,-1].max())
-print('Min:         :',dataset[:,-1].min())
-
-#%%
-#Normalizando os dados
+ 
+# convert series to supervised learning
+def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
+	n_vars = 1 if type(data) is list else data.shape[1]
+	df = DataFrame(data)
+	cols, names = list(), list()
+	# input sequence (t-n, ... t-1)
+	for i in range(n_in, 0, -1):
+		cols.append(df.shift(i))
+		names += [('var%d(t-%d)' % (j+1, i)) for j in range(n_vars)]
+	# forecast sequence (t, t+1, ... t+n)
+	for i in range(0, n_out):
+		cols.append(df.shift(-i))
+		if i == 0:
+			names += [('var%d(t)' % (j+1)) for j in range(n_vars)]
+		else:
+			names += [('var%d(t+%d)' % (j+1, i)) for j in range(n_vars)]
+	# put it all together
+	agg = concat(cols, axis=1)
+	agg.columns = names
+	# drop rows with NaN values
+	if dropnan:
+		agg.dropna(inplace=True)
+	return agg
+ 
+# load dataset
+dataset = read_csv('../data/residential/daily/Residential_1_daily_sum.csv', header=0, index_col=0)
+values = dataset.values
+# =============================================================================
+# # integer encode direction
+# encoder = LabelEncoder()
+# values[:,4] = encoder.fit_transform(values[:,4])
+# =============================================================================
+# ensure all data is float
+values = values.astype('float32')
+# normalize features
 scaler = MinMaxScaler(feature_range=(0, 1))
-dataset = scaler.fit_transform(dataset)
-
-#%%
-#Dividindo entre dados de treinamento e dados de teste
-train_size = int(len(dataset) * 0.7)
-test_size = len(dataset) - train_size
-train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
-
-#%%
-#X = t e Y = t + 1
-look_back = 3
-trainX, trainY = create_dataset(train, input_shape, look_back)
-testX, testY = create_dataset(test, input_shape, look_back)
-
-#%%
-#Mudando a forma dos dados de entrada
-# trainX = numpy.reshape(trainX, (trainX.shape[0], trainX.shape[1], 6))
-# testX = numpy.reshape(testX, (testX.shape[0], testX.shape[1], 6))
-#%%
-trainX = numpy.reshape(trainX, (trainX.shape[0], trainX.shape[1], input_shape))
-testX = numpy.reshape(testX, (testX.shape[0], testX.shape[1], input_shape))
-#%%
-#Criando e treinando a rede LSTM
-batch_size = 1
-
-es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=15)
-
+scaled = scaler.fit_transform(values)
+# frame as supervised learning
+reframed = series_to_supervised(scaled, 1, 1)
+# drop columns we don't want to predict
+reframed.drop(reframed.columns[[-2,-3,-4]], axis=1, inplace=True)
+print(reframed.head())
+ 
+# split into train and test sets
+values = reframed.values
+size = int(reframed.shape[0]*0.7)
+train = values[:size, :]
+test = values[size:, :]
+# split into input and outputs
+train_X, train_y = train[:, :-1], train[:, -1]
+test_X, test_y = test[:, :-1], test[:, -1]
+# reshape input to be 3D [samples, timesteps, features]
+train_X = train_X.reshape((train_X.shape[0], 1, train_X.shape[1]))
+test_X = test_X.reshape((test_X.shape[0], 1, test_X.shape[1]))
+print(train_X.shape, train_y.shape, test_X.shape, test_y.shape)
+ 
+# design network
 model = Sequential()
-model.add(LSTM(4, activation='linear',batch_input_shape=(batch_size, look_back, input_shape), stateful=True, return_sequences=True))
-# model.add(LSTM(4,activation='linear', batch_input_shape=(batch_size, look_back, input_shape), stateful=True))
-model.add(Dense(1,activation='linear'))
-model.compile(loss='mean_squared_error', optimizer='adam')
+model.add(LSTM(50,activation='linear', input_shape=(train_X.shape[1], train_X.shape[2])))
+model.add(Dense(1, activation='linear',))
+model.compile(loss='mae', optimizer='adam')
 
-start = time.time()
+# early stopping
+es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=100)
 
-history = model.fit(trainX, trainY, epochs=3, batch_size=batch_size, verbose=2, shuffle=False,validation_split=0.33,callbacks=[es])
+# fit network
+history = model.fit(train_X, train_y, epochs=500, batch_size=150, validation_data=(test_X, test_y), verbose=2, shuffle=False, callbacks=[es])
 
-model.reset_states()
-    
-print('Train Time:',str(datetime.timedelta(seconds=(time.time() - start))))
+# plot history
+pyplot.plot(history.history['loss'], label='train')
+pyplot.plot(history.history['val_loss'], label='test')
+pyplot.legend()
+pyplot.show()
+ 
+# make a prediction
+yhat = model.predict(test_X)
+test_X = test_X.reshape((test_X.shape[0], test_X.shape[2]))
+# invert scaling for forecast
+inv_yhat = concatenate((yhat, test_X[:, 1:]), axis=1)
+inv_yhat = scaler.inverse_transform(inv_yhat)
+inv_yhat = inv_yhat[:,0]
+# invert scaling for actual
+test_y = test_y.reshape((len(test_y), 1))
+inv_y = concatenate((test_y, test_X[:, 1:]), axis=1)
+inv_y = scaler.inverse_transform(inv_y)
+inv_y = inv_y[:,0]
+# calculate RMSE
+rmse = sqrt(mean_squared_error(inv_y, inv_yhat))
+print('Test RMSE: %.3f' % rmse)
 
-#%%
-#plot train and validation loss
-plt.figure()
-plt.plot(history.history['loss'],label='train')
-plt.plot(history.history['val_loss'],label='validation')
-plt.title('model train vs validation loss')
-plt.ylabel('loss')
-plt.xlabel('epoch')
-plt.legend(['train', 'validation'], loc='upper right')
-plt.show()
+pyplot.plot(inv_y,inv_y)
+pyplot.plot(inv_y,inv_yhat,'o')
+pyplot.show()
 
-#%%
-#Fazendo as previsões
-start = time.time()
-trainPredict = model.predict(trainX, batch_size=batch_size)
-model.reset_states()
-testPredict = model.predict(testX, batch_size=batch_size)
-print('Predict Time:',str(datetime.timedelta(seconds=(time.time() - start))))
-
-#%%
-#Arrumando para inverter
-# =============================================================================
-# trainPredict = numpy.repeat(trainPredict,input_shape,axis=1)
-# testPredict = numpy.repeat(testPredict,input_shape,axis=1)
-# 
-# trainY = numpy.reshape(trainY,(trainY.shape[0],1))
-# trainY = numpy.repeat(trainY,input_shape,axis=1)
-# 
-# testY = numpy.reshape(testY,(testY.shape[0],1))
-# testY = numpy.repeat(testY,input_shape,axis=1)
-# =============================================================================
-
-#%%
-# =============================================================================
-# #Invertendo as previsões
-# trainPredict = scaler.inverse_transform(trainPredict)
-# trainY = scaler.inverse_transform(trainY)
-# testPredict = scaler.inverse_transform(testPredict)
-# testY = scaler.inverse_transform(testY)
-# =============================================================================
-
-#%%
-# Plot scatter Treino
-plt.figure()
-plt.plot(trainY,trainPredict,'o',label='trainPredict')
-plt.plot(trainY,trainY,label='trainY')
-plt.legend()
-plt.title("Train - Scatter")
-plt.xlabel("real")
-plt.ylabel('predicted')
-plt.show()
-
-#%%
-# Plot scatter Treste
-plt.figure()
-plt.plot(testY,testPredict,'o',label='testPredict')
-plt.plot(testY,testY,label='testY')
-plt.title("Test - Scatter")
-plt.xlabel("real")
-plt.ylabel('predicted')
-plt.legend()
-plt.show()
-#%%
-# Plot Treino
-plt.figure()
-plt.plot(trainY,label='trainY')
-plt.plot(trainPredict,label='trainPredict')
-plt.legend()
-plt.title("Train - Plot")
-plt.show()
-
-#%%
-# Plot Teste
-plt.figure()
-plt.plot(testY,label='testY')
-plt.plot(testPredict,label='testPredict')
-plt.title("Test - plot")
-plt.legend()
-plt.show()
-
-#%%
-# =============================================================================
-# #Ajustando os dados de treinamento para serem plotados
-# trainPredictPlot = numpy.empty_like(dataset)
-# trainPredictPlot[:, :] = numpy.nan
-# trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
-# 
-# #%%
-# #Ajustando os dados de teste para serem plotados
-# testPredictPlot = numpy.empty_like(dataset)
-# testPredictPlot[:, :] = numpy.nan
-# testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset)-1, :] = testPredict
-# #%%
-# #Plotando o gráfico final
-# plt.figure()
-# plt.plot(scaler.inverse_transform(dataset))
-# plt.plot(trainPredictPlot)
-# plt.plot(testPredictPlot)
-# plt.grid()
-# plt.show()
-# =============================================================================
+pyplot.plot(inv_y)
+pyplot.plot(inv_yhat)
+pyplot.show()
